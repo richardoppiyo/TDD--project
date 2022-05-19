190b603c850f04f5eac0ed145befc88da102985f
Richard Opiyo <romenyo@dewcis.com>
2022-05-20 01:48:47 +0300

    add the factorial test

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -1,1 +1,12 @@
-require '/.factorial'
+require './solver'
+
+describe 'the factorial of a number' do
+	before :each do
+    	@solver = Solver.new
+  	end
+
+	it 'return the factorial for given number' do
+		# fact = Solver.new.factorial(3)
+		expect(@solver.factorial(3)).to eq(6)
+	end
+end
